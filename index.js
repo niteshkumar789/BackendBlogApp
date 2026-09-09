c9b8d5c923c80fe4dffe50f70c60ddf6792ef0dc
Nitesh Kumar <niteshkumar802183@gmail.com>
2026-09-09 13:36:03 +0530

    changing port:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-const PORTNUMBER = process.env.PORT || 5000;
+const PORTNUMBER = process.env.PORT || 3000;
 
 const blog = require("./routes/blogRouter");
 const { connectToDB } = require("./config/database");
@@ -14,6 +14,6 @@ app.use("/", (req, res) => {
     res.send(`<h1> Hello </h1>`);
 });
 
-app.listen(4000, () => {
+app.listen(3000, () => {
     console.log(`server started on port ${PORTNUMBER}`);
 });
