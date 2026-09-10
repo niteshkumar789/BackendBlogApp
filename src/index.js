57777e243fb5be6790d75f71fb5be0fc3f58c6f2
Nitesh Kumar <niteshkumar802183@gmail.com>
2026-09-10 15:56:20 +0530

    env file

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.use("/", (req, res) => {
     res.send(`<h1> Hello </h1>`);
 });
 
-app.listen(3000, () => {
+app.listen(PORTNUMBER, () => {
     console.log(`server started on port ${PORTNUMBER}`);
 });
